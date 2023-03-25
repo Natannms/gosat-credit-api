19a652f7e177144b6085423a1985423839040fbe
Natannms <agnusnat.nms@gmail.com>
2023-03-24 21:55:43 -0300

    Fix(Offers): mudanto rota de ofertas

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 //Credit Routes
 Route::get('/opportunities/{cpf}', [OpportunitiesController::class, 'getOpportunities']);
-Route::get('/offers/{cpf}', [OfferController::class, 'offer']);
+Route::post('/offers', [OfferController::class, 'offer']);
 
 Route::post('/contract', [LoanController::class, 'store']);
 Route::get('/contract/{id}', [ContractController::class, 'show']);
